ee68d63445693a65fa81714cb699db5a8505eb16
QAwangyang <yang.wang@zenjoy.net>
2017-10-16 11:48:49 +0800

    增加slot5的检查

--- google_test_demo/test_check_food_cost.py
+++ google_test_demo/test_check_food_cost.py
@@ -20,34 +20,34 @@ from oauth2client.file import Storage
 # map4 90行
 dict_name = {
     "map4": {
-        "spreadsheetId_scence": "1HO9ZA93QW-jZBlhLSgF_IKwqBbIpBa1TJURQ1vaULBU",
-        "rangeName_scence": "Timeline!D2:BK33",
+        "spreadsheetId_scence": "16i_V-NmIhIqxFyD8HgeNucV70DYoE3iqjyD3fpqNXkI",
+        "rangeName_scence": "Timeline!D2:BK34",
         "rangeName_scence2": "Timeline!D1:BK34",
-        "spreadsheetId": "1v-eAQHSLTcj2u_msdtkedBB4wrj0uIVVi1e-tKZMeFI",
+        "spreadsheetId": "1mm-sURsigZ3Q_gLgU3RzP14BhljqgyiWED6rPaEM6qI",
         "rangeName": "第一稿-关卡设计!BO5:GM64",
-        "foodName": "食物!B3:B50"
+        "foodName": "食物!B3:B60"
     },
     "map3": {
-        "spreadsheetId_scence": "11SzQ6trYp1DK86z-ktRT0ANr76fmdPiyng5Ts_ICcHU",
-        "rangeName_scence": "Timeline!D2:BK33",
-        "rangeName_scence2": "Timeline!D1:BK34",
-        "spreadsheetId": "1DfqZ5v20o7jgKKP9HQR2k6OnqP10i5B93MpoqD3T10s",
+        "spreadsheetId_scence": "1_zKvv9zotGnuJj0KscltwqaM_lUU_987ihbFlN8oevo",
+        "rangeName_scence": "Timeline-c!D2:BK32",
+        "rangeName_scence2": "Timeline-c!D1:BK34",
+        "spreadsheetId": "1pFQNMChfSSqV1vMCEjM8mw7fqViKd03TseTMs4Mnp-I",
         "rangeName": "第一稿-关卡设计!CE5:HC64",
         "foodName": "食物!B3:B47"
     },
     "map2": {
-        "spreadsheetId_scence": "1n8FjPc78y6TjNmexwVZFBtMqHcGCwTwv99Khw6TLthE",
-        "rangeName_scence": "Timeline!D2:BK33",
-        "rangeName_scence2": "Timeline!D1:BK34",
-        "spreadsheetId": "1lPXokBK3zI8fz_swSBAOa0QRyEX32ypX3PDe7IAyn4g",
+        "spreadsheetId_scence": "16HsFF7iy6w88lUSIUesHes6MYZYEnq4Wy7J-L8z3qTo",
+        "rangeName_scence": "Timeline-c!D2:BK29",
+        "rangeName_scence2": "Timeline-c!D1:BK34",
+        "spreadsheetId": "1SmQNevKYlJzJht8gsnhA29CC74Qav35TDALVf4_zFKI",
         "rangeName": "第一稿-关卡设计!CN5:GR64",
         "foodName": "食物!B3:B47"
     },
     "map1": {
-        "spreadsheetId_scence": "1jcZfcQn823qbn3XlcuZJJOa0fkZFOXwssYzAJc_M4F8",
-        "rangeName_scence": "Timeline!D2:BK33",
-        "rangeName_scence2": "Timeline!D1:BK34",
-        "spreadsheetId": "1tOnLTdR_m94ioN0xjQsmtVKvAxOpULMKytUucbj4-D0",
+        "spreadsheetId_scence": "1JzGhU8JBgBx47IZ7DXPhb4sUZnly1t-OSqGQCNmna9E",
+        "rangeName_scence": "Timeline-c!D2:BK29",
+        "rangeName_scence2": "Timeline-c!D1:BK34",
+        "spreadsheetId": "1NY4V8naN-LsnkBELWHyPerBOarBg3VSev6ahMBct7iY",
         "rangeName": "第一稿-关卡设计!BQ5:FU64",
         "foodName": "食物!B3:B47"
     }
@@ -59,41 +59,42 @@ dict_name = {
 
 def check_upgrade():
     dict_value = {}
-    for i in range(1, 90):
+    for i in range(1, 45):
         dict_value[i] = {}
-
-    # spreadsheet_id = "1jcZfcQn823qbn3XlcuZJJOa0fkZFOXwssYzAJc_M4F8"  # map1
-    spreadsheet_id = "1n8FjPc78y6TjNmexwVZFBtMqHcGCwTwv99Khw6TLthE"  # map2
-    # spreadsheet_id = "11SzQ6trYp1DK86z-ktRT0ANr76fmdPiyng5Ts_ICcHU"  # map3
-    # spreadsheet_id = "1HO9ZA93QW-jZBlhLSgF_IKwqBbIpBa1TJURQ1vaULBU"  # map4
+# 1HO9ZA93QW - jZBlhLSgF_IKwqBbIpBa1TJURQ1vaULBU
+# spreadsheet_id = "1JzGhU8JBgBx47IZ7DXPhb4sUZnly1t-OSqGQCNmna9E"  #map1
+# spreadsheet_id = "16HsFF7iy6w88lUSIUesHes6MYZYEnq4Wy7J-L8z3qTo"  # map2
+# spreadsheet_id = "1_zKvv9zotGnuJj0KscltwqaM_lUU_987ihbFlN8oevo"  # map3
+    # spreadsheet_id = "16i_V-NmIhIqxFyD8HgeNucV70DYoE3iqjyD3fpqNXkI"  # map4
+    spreadsheet_id = "1jndZFK4gXLrwbl3AnOgjyXqKBJW2NHxk8gG_318y5kQ"  # slot5
     range_names = []
-    range_names.append("upgrade!B4:B93")  # map1 94
-    range_names.append("upgrade!c4:c93")  # map4 92
-    range_names.append("upgrade!i4:I93")  # map2 93
-    range_names.append("upgrade!j4:j93")  # map3 90
-    range_names.append("upgrade!m4:m93")
+    range_names.append("upgrade!B4:B48")  # map1 83
+    range_names.append("upgrade!c4:c48")  # map4 93
+    range_names.append("upgrade!i4:I48")  # map2 81
+    range_names.append("upgrade!j4:j48")  # map3 90
+    # range_names.append("upgrade!m4:m48")  # 经验
     service = google_get_credentials.get_service()
     re = service.spreadsheets().values().batchGet(spreadsheetId=spreadsheet_id, ranges=range_names).execute()
     result = re.get('valueRanges', [])
 
-    for i in range(1, 90):
+    for i in range(1, 45):
         row_value = []
         for line in result:
             value = line.get('values', [])
-            # print value
+            # print value, len(value)
             row_value.append(value[i])
         dict_value[i]["id"] = row_value[0]
         dict_value[i]["level"] = row_value[1]
         dict_value[i]["cash"] = row_value[2]
         dict_value[i]["diamond"] = row_value[3]
-        dict_value[i]["exp"] = row_value[4]
+        #dict_value[i]["exp"] = row_value[4]
 
     return dict_value
 
 
 def read_test_file():
     test_info = []
-    f = open("D:/map2")
+    f = open("slot5map1.log")
     for line in f.readlines():
         if 'UpgradeMan.js' and "testinfo:" in line:
             line = line.split("testinfo: ")
@@ -118,11 +119,14 @@ def main():
             # print test_value
             # print upgrade_google[i]
             if test_value.get("id") == int(upgrade_google[i]["id"][0]) and test_value.get("level") == int(upgrade_google[i]["level"][0]):
-                if(test_value.get("exp") == int(upgrade_google[i]["exp"][0]) and test_value.get("diamond") == int(upgrade_google[i]["diamond"][0]) and test_value.get("cash") == int(upgrade_google[i]["cash"][0])):
+                # if(test_value.get("exp") == int(upgrade_google[i]["exp"][0]) and
+                # test_value.get("diamond") == int(upgrade_google[i]["diamond"][0]) and
+                # test_value.get("cash") == int(upgrade_google[i]["cash"][0])):
+                if(test_value.get("diamond") == int(upgrade_google[i]["diamond"][0]) and test_value.get("cash") == int(upgrade_google[i]["cash"][0])):
                     print str(upgrade_google[i]["id"]) + "ok"
                 else:
                     # print "数值表中id:" + str(upgrade_google[i]["id"][0]) + ",level:" + str(test_value.get("level")) + "的经验为" + str(upgrade_google[i]["exp"][0])
-                    print "id为" + str(test_value.get("id")) + "等级为" + str(test_value.get("level")) + "的表经验" + str(upgrade_google[i]["exp"][0]) + ":游戏中" + str(test_value.get("exp"))
+                   # print "id为" + str(test_value.get("id")) + "等级为" + str(test_value.get("level")) + "的表经验" + str(upgrade_google[i]["exp"][0]) + ":游戏中" + str(test_value.get("exp"))
                     print "id为" + str(test_value.get("id")) + "等级为" + str(test_value.get("level")) + "的表钻石" + str(upgrade_google[i]["diamond"][0]) + ":游戏中" + str(test_value.get("diamond"))
                     print "id为" + str(test_value.get("id")) + "等级为" + str(test_value.get("level")) + "的表金币" + str(upgrade_google[i]["cash"][0]) + ":游戏中" + str(test_value.get("cash"))
 
