1e165ee5782866e584b7c395580b8196c6aa958b
QAwangyang <yang.wang@zenjoy.net>
2018-01-23 21:50:34 +0800

    更新secret

--- tools/cooking_sha1.py
+++ tools/cooking_sha1.py
@@ -3,17 +3,19 @@
 import hashlib
 import sys
 import base64
+import json
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
-secret_cooking3 = "foobar"  # 测试服
-secret_cooking2 = ""  # 测试服
+init_secret = "foobar"  # 初始化
 
 
-def cooking_sha1(strings, secret=secret_cooking2):
+def cooking_sha1(strings, secret=init_secret):
 
     strings = (strings + secret)
     sha1 = hashlib.sha1()
     sha1.update(strings.encode('utf-8'))
     res = sha1.hexdigest()
     return res
+
+#print (json.dumps(contents.deviceId))
